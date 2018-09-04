0feccc3f5f687907d976351cc06aa142b6a8e275
Rick Coates <rick@jacketscreative.com>
2018-09-04 14:50:14 -0300

    Fix to Events Calendar PRO bug with multiple Google API includes.

--- functions.php
+++ functions.php
@@ -9,4 +9,10 @@ function salient_child_enqueue_styles() {
    		wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
 }
 
+function tribe_events_map_apis() {
+    if (tribe_is_event() || is_singular( 'tribe_events' ));
+    wp_dequeue_script( 'tribe-events-pro-geoloc' );
+}
+add_action( 'wp_enqueue_scripts', 'tribe_events_map_apis' );
+
 ?>
